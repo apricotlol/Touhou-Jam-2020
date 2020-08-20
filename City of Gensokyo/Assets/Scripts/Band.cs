eabfbaac94474beaeae6b6681fefcd6aad8a3956
Sam <sam.urigav@gmail.com>
2020-08-20 17:30:22 -0400

    Create Band a nd TBB Objects

---
+++ City of Gensokyo/Assets/Scripts/Band.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Band : MonoBehaviour
+{
+
+    private int health;
+    private int energy;
+
+    public Band()
+    {
+        health = 200;
+        energy = 100;
+    }
+
+    public int getEnergy()
+    {
+        return energy;
+    }
+
+    public void updateEnergy(int updateValue)
+    {
+        energy = energy + updateValue;
+    }
+
+    public void updateHealth(int updateValue)
+    {
+        health = health + updateValue;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
